d5c3ba9afbadb22e58fec147bb9ac1e4db1f256a
ThiccPan <ihilmiderian@gmail.com>
2024-10-12 00:02:44 +0700

    feat: generate spec code

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 # oapi-codegen --config=./api/openapi/apiconfig.yml ./api/openapi/users.yml
 
-oapi-codegen -generate types,skip-prune  -o internal/users/openapi_types.gen.go  -package main ./api/openapi/users.yml
-oapi-codegen -generate chi-server,skip-prune -o internal/users/openapi_server.gen.go -package main ./api/openapi/users.yml
-# oapi-codegen -generate spec,skip-prune   -o openapi_spec.gen.go   -package api ./api/openapi/users.yml
+oapi-codegen -generate types,skip-prune  -o internal/users/openapi_types.gen.go  -package main ./internal/users/api/openapi/users.yml
+oapi-codegen -generate chi-server,skip-prune -o internal/users/openapi_server.gen.go -package main ./internal/users/api/openapi/users.yml
+oapi-codegen -generate spec,skip-prune   -o openapi_spec.gen.go   -package main ./internal/users/api/openapi/users.yml
 # oapi-codegen -generate client,skip-prune -o openapi_client.gen.go -package api flamenco-openapi.yaml
